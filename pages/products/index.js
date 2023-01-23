d6d1040a11cf8a6e20d6f03374dd2dd50499cebc
ryuimoto <narrow617@gmail.com>
2023-01-23 14:47:30 +0900

    e text

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,4 +1,12 @@
+import styles from '@/styles/Home.module.css'
+
 export default function ProductsList(){
-    return <h1>Hello</h1>;
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h2 className={styles.title}>商品一覧</h2>
+            </main>
+        </div>
+    );
 }
 
